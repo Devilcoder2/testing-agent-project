2a77cb4907ddcd537921b94754374104d2aa72cb
Ramandeep Singh <113927776+Devilcoder2@users.noreply.github.com>
2026-08-06 00:06:06 +0530

    Declare Selenium driver types

---
+++ types/selenium-webdriver.d.ts
@@ -0,0 +1,11 @@
+declare module "selenium-webdriver" {
+  export const Builder: new () => {
+    usingServer(url: string): {
+      forBrowser(name: string): { build(): Promise<ThenableWebDriver> };
+    };
+  };
+  export type ThenableWebDriver = {
+    get(url: string): Promise<void>;
+    executeScript(script: string): Promise<unknown>;
+  };
+}
