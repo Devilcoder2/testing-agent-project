6bd2094c508d6a87be9c99c344c8985936f79aec
Ramandeep Singh <113927776+Devilcoder2@users.noreply.github.com>
2026-08-08 19:10:39 +0530

    Add recording creation route

---
+++ app/recordings/new/page.tsx
@@ -0,0 +1,6 @@
+import { AppShell } from "../../../components/app-shell";
+import { NewRecordingView } from "../../../components/sentinel-views";
+
+export default function NewRecordingPage() {
+  return <AppShell><NewRecordingView /></AppShell>;
+}
