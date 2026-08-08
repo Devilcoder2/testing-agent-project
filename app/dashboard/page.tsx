02321fd8f4f8c97e0497b5e5d84c4bc83745aa71
Ramandeep Singh <113927776+Devilcoder2@users.noreply.github.com>
2026-08-08 19:09:32 +0530

    Add Sentinel dashboard route

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,6 @@
+import { AppShell } from "../../components/app-shell";
+import { DashboardView } from "../../components/sentinel-views";
+
+export default function DashboardPage() {
+  return <AppShell><DashboardView /></AppShell>;
+}
