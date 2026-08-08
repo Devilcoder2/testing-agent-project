b90211b17faeac9cc6d4da89b006bcb94e5705bc
Ramandeep Singh <113927776+Devilcoder2@users.noreply.github.com>
2026-08-08 19:22:55 +0530

    Wrap recording route search params

--- app/recordings/new/page.tsx
+++ app/recordings/new/page.tsx
@@ -1,6 +1,7 @@
+import { Suspense } from "react";
 import { AppShell } from "../../../components/app-shell";
 import { NewRecordingView } from "../../../components/sentinel-views";
 
 export default function NewRecordingPage() {
-  return <AppShell><NewRecordingView /></AppShell>;
+  return <AppShell><Suspense fallback={null}><NewRecordingView /></Suspense></AppShell>;
 }
