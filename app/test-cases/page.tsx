3f92be035f82a5a9c377fd26d0fdf2b320b5e183
Ramandeep Singh <113927776+Devilcoder2@users.noreply.github.com>
2026-08-08 19:09:52 +0530

    Add Test Case inventory route

---
+++ app/test-cases/page.tsx
@@ -0,0 +1,6 @@
+import { AppShell } from "../../components/app-shell";
+import { TestCasesView } from "../../components/sentinel-views";
+
+export default function TestCasesPage() {
+  return <AppShell><TestCasesView /></AppShell>;
+}
