fae40e4de40fcd18e84206d31e8516bc68b59577
Ramandeep Singh <113927776+Devilcoder2@users.noreply.github.com>
2026-08-08 23:24:23 +0530

    Render recording workspace without app shell

--- app/recordings/[id]/page.tsx
+++ app/recordings/[id]/page.tsx
@@ -1,10 +1,9 @@
 "use client";
 
 import { useParams } from "next/navigation";
-import { AppShell } from "../../../components/app-shell";
 import { RecordingWorkspaceView } from "../../../components/sentinel-views";
 
 export default function RecordingWorkspacePage() {
   const params = useParams<{ id: string }>();
-  return <AppShell><RecordingWorkspaceView recordingId={params.id} /></AppShell>;
+  return <RecordingWorkspaceView recordingId={params.id} />;
 }
