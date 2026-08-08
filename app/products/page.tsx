419e1a6b2c810043e2d2a98c38a16fe0b88161ef
Ramandeep Singh <113927776+Devilcoder2@users.noreply.github.com>
2026-08-08 19:40:22 +0530

    Add dedicated Products route

---
+++ app/products/page.tsx
@@ -0,0 +1,6 @@
+import { AppShell } from "../../components/app-shell";
+import { ProductsView } from "../../components/sentinel-views";
+
+export default function ProductsPage() {
+  return <AppShell><ProductsView /></AppShell>;
+}
