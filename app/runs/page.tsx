bc467764f9da969410a6bc42e3bb63e9fd9f3152
Ramandeep Singh <113927776+Devilcoder2@users.noreply.github.com>
2026-08-09 15:47:19 +0530

    feat: add runs inventory route

---
+++ app/runs/page.tsx
@@ -0,0 +1,6 @@
+import { AppShell } from "../../components/app-shell";
+import { RunsView } from "../../components/sentinel-views";
+
+export default function RunsPage() {
+  return <AppShell><RunsView /></AppShell>;
+}
