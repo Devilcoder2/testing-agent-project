9cc2f756f2cd516f8380897185eb338888691a5b
Ramandeep Singh <113927776+Devilcoder2@users.noreply.github.com>
2026-08-11 00:05:55 +0530

    fix: suspend test case filters during render

--- app/test-cases/page.tsx
+++ app/test-cases/page.tsx
@@ -1,6 +1,7 @@
+import { Suspense } from "react";
 import { AppShell } from "../../components/app-shell";
 import { TestCasesView } from "../../components/sentinel-views";
 
 export default function TestCasesPage() {
-  return <AppShell><TestCasesView /></AppShell>;
+  return <AppShell><Suspense><TestCasesView /></Suspense></AppShell>;
 }
