77c5a1385f14e1c3c79487e7087b487f5c34e77f
Ramandeep Singh <113927776+Devilcoder2@users.noreply.github.com>
2026-08-15 18:19:06 +0530

    feat: add releases inventory route

---
+++ app/releases/page.tsx
@@ -0,0 +1,6 @@
+import { AppShell } from "../../components/app-shell";
+import { ReleasesView } from "../../components/release-views";
+
+export default function ReleasesPage() {
+  return <AppShell><ReleasesView /></AppShell>;
+}
