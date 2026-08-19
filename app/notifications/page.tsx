c469e47db8866495bd947bc9bbe8eda707245192
Ramandeep Singh <113927776+Devilcoder2@users.noreply.github.com>
2026-08-20 01:01:11 +0530

    feat: add notifications route

---
+++ app/notifications/page.tsx
@@ -0,0 +1,6 @@
+import { AppShell } from "../../components/app-shell";
+import { NotificationsView } from "../../components/notification-views";
+
+export default function NotificationsPage() {
+  return <AppShell><NotificationsView /></AppShell>;
+}
