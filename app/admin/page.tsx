82358cedf70d7ab11ae47a9aabeb70af64b436cf
Ramandeep Singh <113927776+Devilcoder2@users.noreply.github.com>
2026-08-23 22:17:22 +0530

    feat: add organization administration route

---
+++ app/admin/page.tsx
@@ -0,0 +1,6 @@
+import { AdministrationView } from "@/components/admin-views";
+import { AppShell } from "@/components/app-shell";
+
+export default function AdministrationPage() {
+  return <AppShell><AdministrationView /></AppShell>;
+}
