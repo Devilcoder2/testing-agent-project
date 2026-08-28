bc9b008e8201096311d975dc64eb4d45e8fcde79
Ramandeep Singh <113927776+Devilcoder2@users.noreply.github.com>
2026-08-28 21:31:06 +0530

    feat: add Telegram account integrations route

---
+++ app/account/integrations/page.tsx
@@ -0,0 +1,6 @@
+import { AppShell } from "@/components/app-shell";
+import { TelegramIntegrationsView } from "@/components/telegram-views";
+
+export default function AccountIntegrationsPage() {
+  return <AppShell><TelegramIntegrationsView /></AppShell>;
+}
