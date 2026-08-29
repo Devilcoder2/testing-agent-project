55bcbc0e07c053373721fe4b2b65d35311e66f4a
Ramandeep Singh <113927776+Devilcoder2@users.noreply.github.com>
2026-08-29 09:21:20 +0530

    Route Test Data to the tabular workspace

--- app/test-data/page.tsx
+++ app/test-data/page.tsx
@@ -1,6 +1,6 @@
 import { Suspense } from "react";
 import { AppShell } from "@/components/app-shell";
-import { TestDataView } from "@/components/sentinel-views";
+import { TestDataView } from "@/components/test-data-view";
 
 export default function TestDataPage() {
   return <AppShell><Suspense><TestDataView /></Suspense></AppShell>;
