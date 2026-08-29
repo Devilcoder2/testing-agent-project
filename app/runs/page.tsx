d777672df6ae96bf4f2a09ac24fa9fe22668d51b
Ramandeep Singh <113927776+Devilcoder2@users.noreply.github.com>
2026-08-29 09:46:06 +0530

    Suspend Runs query feedback during prerender

--- app/runs/page.tsx
+++ app/runs/page.tsx
@@ -1,6 +1,7 @@
+import { Suspense } from "react";
 import { AppShell } from "../../components/app-shell";
 import { RunsView } from "../../components/sentinel-views";
 
 export default function RunsPage() {
-  return <AppShell><RunsView /></AppShell>;
+  return <AppShell><Suspense><RunsView /></Suspense></AppShell>;
 }
